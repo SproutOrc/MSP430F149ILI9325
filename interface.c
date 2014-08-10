a095cadadfeb1e7723a98daca1235df3778e5f20
SproutOrc <orcatropos@gmail.com>
2014-08-10 23:57:11 +0800

    INI add FFT can show FFT

--- interface.c
+++ interface.c
@@ -3,5 +3,9 @@
 
 void MAIN_WINDOW()
 {
-    GUI_sprintf_string(0, 50, "xiao mi mi !", RED, WHITE);
+    // GUI_sprintf_string(0, 50, "xiao mi mi !", RED, WHITE);
+    int i;
+    for (i = 0; i < 64; i++) {
+        GUI_box(0, i * 5, 3 * i + 4, i * 5 + 3, 0x001f);//»­ÊµÐÄ¾ØÐÎ
+    }
 }
